5b4e085b6822fd696236d48cc6756c83f97b1b1f
sefa oz <sefaoz@karatguc.com>
2025-03-11 18:52:24 +0300

    xxxx

--- src/app/api/auth/register/vehicle-owner/route.ts
+++ src/app/api/auth/register/vehicle-owner/route.ts
@@ -16,8 +16,8 @@ export async function POST(request: Request) {
     const cookieStore = await cookies();
 
     const supabase = createServerClient(
-      process.env.NEXT_PUBLIC_SUPABASE_URL!,
-      process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+      process.env.SUPABASE_PROJECT_URL!,
+      process.env.SUPABASE_API_KEY!,
       {
         cookies: {
           get(name: string) {
